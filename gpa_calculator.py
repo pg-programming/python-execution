c5cba416512c81f1340bdafdd8ffc737316ec352
Reed Wilson <reedcwilson@gmail.com>
2015-12-22 22:21:47 -0700

    cleanup the homework

--- gpa_calculator.py
+++ gpa_calculator.py
@@ -1,10 +1,36 @@
 
-# choose n number of classes (n being the number you choose to calculate)
+# the points per letter grade
+a = 4
+a_minus = 3.7
 
-# create variables storing each of the grades and their credits (a: 4, a-: 3.7 etc.)
+# the credits for each class (you could make them more specific if you want)
+credits = 3
 
-# calculate the points that each grade represents (grade * credits)
+# the number of classes we are using
+classes_num = 1
 
-# calculate the gpa total_points / total_credits
+# the letter grade for the class
+biology = raw_input("what grade did you get in biology (a, a-, b+, etc)?")
 
-# print your cumulative GPA
+# keep track of the total number of points from every class operation
+total_points = 0
+
+# declaring this variable outside of the if loop so that it can be used later
+# if it were only inside the if loop then what happens if biology != 'a'? We 
+# wouldn't be able to use it.
+biology_points = 0
+if (biology == 'a'):
+    # store the result of this operation because we are going to use it twice
+    points = a * credits
+    biology = points
+    # there is a += operator that is the same as: total_points = total_points + points
+    # its a shortcut
+    total_points += points
+else:
+    print "I didn't understand that"
+
+# calculate the total credits
+total_credits = credits * classes_num
+
+# print out the GPA
+print "your GPA is: " + str(total_points / total_credits)
--- tip_calculator.py
+++ tip_calculator.py
@@ -1,13 +1,7 @@
 
-# Ask for the server's name (collect the response in a variable)
-
-# Ask for how much the meal was (this should be an integer)
-
-# Ask for how much the tax is (this should be a floating point)
-
-# Ask for what the percentage of the tip is (this should be an integer) (NOTE:
-# percentages are typically represented as numbers like 15 or 20 but if you
-# remember, that is not their real values. Their real values are 0.15 and 0.20).
-# How will you convert your integer into the smaller version?
-
-# Print out the name of the server and the total amount you will pay
+tip = 20.0
+total = 30.0
+server = 'John'
+print "The tip amount is: $" + str(tip)
+print "The total amount for my meal is: $" + str(total)
+print "Thanks for your service " + server
